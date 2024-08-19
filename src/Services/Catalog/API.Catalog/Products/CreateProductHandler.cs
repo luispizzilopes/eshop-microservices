3d33a5852c033d7eddaaf45e4f1a7744381ed7e0
luispizzilopes <luisfelipe1203lf@gmail.com>
2024-08-19 20:30:38 -0300

    first commit - upload project

---
+++ src/Services/Catalog/API.Catalog/Products/CreateProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace API.Catalog.Products;
+
+public record CreateProductCommand 
+(
+    string Name, 
+    List<string> Category, 
+    string Description, 
+    string ImageFile, 
+    decimal Price
+) : IRequest<CreateProductResult>; 
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //Lógica de negócio para criar o produto
+        throw new NotImplementedException();
+    }
+}
